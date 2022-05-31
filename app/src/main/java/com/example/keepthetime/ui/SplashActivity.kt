73eeb7e324549fd4c59139d27ca3e7b2a83f4381
DK <spol@naver.com>
2022-05-31 20:34:58 +0900

    스플래시 액티비티 > 로그인 액티비티 연결

--- app/src/main/java/com/example/keepthetime/ui/SplashActivity.kt
+++ app/src/main/java/com/example/keepthetime/ui/SplashActivity.kt
@@ -1,12 +1,33 @@
 package com.example.keepthetime.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.keepthetime.BaseActivity
 import com.example.keepthetime.R
+import com.example.keepthetime.ui.main.LoginActivity
 
-class SplashActivity : AppCompatActivity() {
+
+class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        val myHandler = Handler(Looper.getMainLooper())
+        myHandler.postDelayed({
+            val myIntent: Intent
+            myIntent = Intent(mContext, LoginActivity::class.java)
+            startActivity(myIntent)
+        }, 2500)
     }
 }

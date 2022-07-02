f5eb6d6da15445903641e8bb557e12d9f07a62dc
DK <spol@naver.com>
2022-07-02 20:08:37 +0900

    친구 추가 및 목록을 보여줄 액티비티 생성

--- app/src/main/java/com/example/keepthetime/ui/settings/MyFriendsActivity.kt
+++ app/src/main/java/com/example/keepthetime/ui/settings/MyFriendsActivity.kt
@@ -2,11 +2,25 @@ package com.example.keepthetime.ui.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.keepthetime.BaseActivity
 import com.example.keepthetime.R
+import com.example.keepthetime.databinding.ActivityMyFriendsBinding
 
-class MyFriendsActivity : AppCompatActivity() {
+class MyFriendsActivity : BaseActivity() {
+
+    lateinit var binding : ActivityMyFriendsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_friends)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_my_friends)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }

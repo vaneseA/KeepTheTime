2c290dcb4c9fc432ac244c59cb573587783d1120
DK <spol@naver.com>
2022-07-02 13:15:11 +0900

    스플래시 액티비티에서 자동 로그인 기능 구현

--- app/src/main/java/com/example/keepthetime/ui/SplashActivity.kt
+++ app/src/main/java/com/example/keepthetime/ui/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.example.keepthetime.ui
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,10 +8,20 @@ import android.os.Handler
 import android.os.Looper
 import com.example.keepthetime.BaseActivity
 import com.example.keepthetime.R
+import com.example.keepthetime.models.BasicResponse
 import com.example.keepthetime.ui.main.LoginActivity
+import com.example.keepthetime.ui.main.MainActivity
+import com.example.keepthetime.utils.ContextUtil
+import com.example.keepthetime.utils.GlobalData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class SplashActivity : BaseActivity() {
+
+    var isTokenOk = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
@@ -19,14 +30,37 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
-apiList.getRequestMyInfo()
+        apiList.getRequestMyInfo(ContextUtil.getLoginToken(mContext)).enqueue(object : Callback<BasicResponse>{
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+                    val br = response.body()!!
+
+                    isTokenOk = true
+                    GlobalData.loginUser = br.data.user
+                    //서버에서 주는 응답 확인 필수
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        })
     }
 
     override fun setValues() {
         val myHandler = Handler(Looper.getMainLooper())
+
         myHandler.postDelayed({
+
             val myIntent: Intent
-            myIntent = Intent(mContext, LoginActivity::class.java)
+
+            if(isTokenOk && ContextUtil.getAutoLogin(mContext)){
+                myIntent = Intent(mContext, MainActivity::class.java)
+            }
+            else{
+                myIntent = Intent(mContext, LoginActivity::class.java)
+
+            }
             startActivity(myIntent)
             finish()
         }, 2500)
--- app/src/main/java/com/example/keepthetime/utils/ContextUtil.kt
+++ app/src/main/java/com/example/keepthetime/utils/ContextUtil.kt
@@ -7,6 +7,7 @@ class ContextUtil {
     companion object {
         private val prefName = "KeepTheTime"
         private val LOGIN_TOKEN = "LOGIN_TOKEN"
+        private val AUTO_LOGIN = "AUTO_LOGIN"
 
         fun setLoginToken(context: Context, token: String) {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
@@ -16,6 +17,17 @@ class ContextUtil {
         fun getLoginToken (context: Context) : String {
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
             return pref.getString(LOGIN_TOKEN, "")!!
+        }
+
+        fun setAutoLogin (context: Context, autoLogin : Boolean){
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
+
+        }
+        fun getAutoLogin (context: Context) : Boolean {
+            val pref = context.getSharedPreferences(prefName,Context.MODE_PRIVATE)
+            return  pref.getBoolean(AUTO_LOGIN, false)
         }
         fun clear (context: Context){
             val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)

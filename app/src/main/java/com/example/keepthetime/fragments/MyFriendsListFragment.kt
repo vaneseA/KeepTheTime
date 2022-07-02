0431c3043cbf13143061f3b6beba1ccfa3b90f57
DK <spol@naver.com>
2022-07-02 20:36:09 +0900

    ViewPager에 올려줄 Fragment 2개 생성

---
+++ app/src/main/java/com/example/keepthetime/fragments/MyFriendsListFragment.kt
@@ -0,0 +1,37 @@
+package com.example.keepthetime.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.keepthetime.R
+import com.example.keepthetime.databinding.FragmentSettingsBinding
+
+class MyFriendsListFragment: BaseFragment() {
+    lateinit var binding: FragmentSettingsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,
+        R.layout.fragment_my_friends_list, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}

eca2d6d4760e3abbbb164c1dfeec06d40264bafe
DK <spol@naver.com>
2022-07-03 17:15:24 +0900

    친구 검색 activity_add_friends.xml 작성

--- app/src/main/java/com/example/keepthetime/models/DataResponse.kt
+++ app/src/main/java/com/example/keepthetime/models/DataResponse.kt
@@ -3,5 +3,6 @@ package com.example.keepthetime.models
 class DataResponse(
     val user: UserData,
     val token: String,
+    val users : List<UserData>
 ) {
 }
---
+++ app/src/main/java/com/example/keepthetime/ui/settings/AddFriendsActivity.kt
@@ -0,0 +1,13 @@
+package com.example.keepthetime.ui.settings
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.keepthetime.R
+
+class AddFriendsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_friends)
+    }
+}
